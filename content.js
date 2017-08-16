92e6cb7d890e43a30242b025c684bd31f060c9a2
Isabelle Li <ili@wellesley.edu>
2017-08-15 22:13:26 -0400

    new way of doing things

--- content.js
+++ content.js
@@ -3,13 +3,34 @@
 
 var cat = chrome.extension.getURL('cat.png');
 
-var observer = new MutationObserver(function(mutation) {
-    console.log('hi');
-    $('img').each(function() {
-        $(this).attr("src", cat);
+$(document).ready(function() {
+    changeCat();
+})
 
-    })
-});
+document.onscroll = function(){
+    changeCat();
+}
 
-var config = {attributes: true, childList:true};
-observer.observe(document.body, config);
+function changeCat() {
+    $('img').attr("src", cat);
+
+    //for each is unnecessary because jquery already selects all of the img elements
+
+    // $('img').each(function() {
+    //     $(this).attr("src", cat);
+    // })
+}
+
+//Old way to observe changes in the DOM
+//Problem with MutationObserver is that it took a while for it to observe changes
+//onscroll is more reactive
+
+// var observer = new MutationObserver(function(mutation) {
+//     $('img').each(function() {
+//         $(this).attr("src", cat);
+//
+//     })
+// });
+//
+// var config = {attributes: true, childList:true};
+// observer.observe(document.body, config);
